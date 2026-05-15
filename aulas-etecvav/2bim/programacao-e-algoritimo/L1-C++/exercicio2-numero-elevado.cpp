be7f38c731cec0d5ac57626bdcdf5f6bcc2c992c
MFMation <mfmation@gmail.com>
2026-05-15 13:19:43 -0300

    Update exercicio2-numero-elevado.cpp

--- aulas-etecvav/2bim/programacao-e-algoritimo/L1-C++/exercicio2-numero-elevado.cpp
+++ aulas-etecvav/2bim/programacao-e-algoritimo/L1-C++/exercicio2-numero-elevado.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cmath>
 int main() {
-    int pot, i, res;
+    int pot = 0, i = 1, res = 0;
     std::cout << "este codigo calcula potencias de 2^0 até 2^n \n";
     std::cout << "digite um numero desejado: ";
     std::cin >> pot;
     std::cout <<"2^0 = 1\n";
-    for(i=1; i<=pot; i++){
+    for(i; i<=pot; i++){
         res = std::pow(2, i);
     std::cout <<"2^"<<i<<" = "<<res<< "\n";
     }
