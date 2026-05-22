c8ea6b62a67c6057244c2694f6314b240847c28d
SebastianSierraDiaz <thesebas320@gmail.com>
2026-05-22 12:05:31 -0300

    Update exercicio1-numero-primo.cpp

--- aulas-etecvav/2bim/programacao-e-algoritimo/L1-C++/exercicio1-numero-primo.cpp
+++ aulas-etecvav/2bim/programacao-e-algoritimo/L1-C++/exercicio1-numero-primo.cpp
@@ -1,3 +1,7 @@
+// Matheus Formigoni Mation
+// 22.05.2026
+// O que o programa faz
+// Lê um número inteiro, e verifica e exibir se o número é primo.
 #include <iostream>
 
 int main() {
@@ -10,6 +14,12 @@ int main() {
          std::cout << i << " é divisivel por "<<num<<" \n";
       }    
     }
-    std::cout << num <<" é divisivel por "<< div<<" numeros";
+    std::cout << num <<" é divisivel por "<< div<<" numeros \n";
+    
+        if ( div == 2 )
+    {
+        std::cout << num <<" é um numero PRIMO pois tem 2 divisores (1 e "<<num<<")";
+            }
+            
     return 0;
 }
